d2d8c42dfa5db5b302d615f2d2aa238d12047ebf
MOHAMMED SAAD <saadms00000@gmail.com>
2025-11-04 14:42:31 +0530

    FetchAPIUpdate

--- 7_fetch_API.js
+++ 7_fetch_API.js
@@ -1,6 +1,33 @@
 //fetch API data using Fetch API and handle promises
 console.log("Fetch API Practice File");
+const URL = "https://catfact.ninja/facts";
+const factPara = document.getElementById("para")
+const btn = document.querySelector("#btn")
 
-const URL = "https://cat-fact.herokuapp.com/facts";
+// const getFacts = async() => {//async function to use await
+//     console.log("calling...........")
+//     const promise = await fetch(URL);//waiting for fetch to complete
+//     console.log(promise);
+//     const Data = await promise.json();//waiting for json conversion
+//     console.log("Data fetched from API:");
+//     console.log(Data.data[1].fact);//logging the 'data' array from the response
+//     const x = Data.data[1].fact
+//     factPara.innerText=x
+// }
 
-// Function to fetch data from the API 
+function getFacts(){
+    fetch(URL)
+    .then((response)=>{ 
+        data = response.json()
+        return data
+    })
+    .then((data)=>{
+        console.log(data);
+        factPara.innerText=data.data[0].fact
+    })
+}
+
+btn.addEventListener("click",getFacts);
+
+
+//End of Fetch API Practice File
